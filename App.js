52f48604a119db69ed1dc1132f04fe9458485af5
Sidharth D <sidharth363@gmail.com>
2018-02-25 22:59:33 +0530

    Data persistance

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { KeyboardAvoidingView, Text, StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, Text, StyleSheet, AsyncStorage } from 'react-native';
 
 import Header from './components/Header.js';
 import ToDoList from './components/ToDoList.js';
@@ -9,32 +9,58 @@ export default class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {
-      todos: []
+    this.state = { todos: [] }
+  }
+
+  componentWillMount() {
+    this.load();
+  }
+
+  save = async () => {
+    try {
+      await AsyncStorage.setItem('TODOS', JSON.stringify(this.state));
+    } catch (e) {
+      console.error('Could not save todos');
+    }
+  }
+
+  load = async () => {
+    try {
+      const todos = await AsyncStorage.getItem('TODOS');
+      if (todos !== null) {
+        this.setState({ todos: JSON.parse(todos).todos })
+      }
+    } catch (e) {
+      console.error('Could not load todos');
     }
   }
 
   addToDo = text => {
+    let { todos } = this.state;
     newToDo = {
       key: new Date(),
       todo: text,
       pending: true
     }
+    todos.push(newToDo)
     this.setState({
-      todos: [newToDo, ...this.state.todos]
+      todos
     })
+    this.save();
   }
 
   toggleStatus = index => {
     let { todos } = this.state;
     todos[index].pending = !todos[index].pending;
     this.setState({ todos })
+    this.save();
   }
 
   removeToDo = index => {
     let { todos } = this.state;
     todos.splice(index, 1)
     this.setState({ todos });
+    this.save();
   }
 
   render() {
